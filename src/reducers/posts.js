057ef3951b7703b1e733851dde26e594f138c514
carsalcedo <fercarrera07@gmail.com>
2023-03-13 11:24:45 -0400

    client finished

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-import { CREATE, DELETE, END_LOADING, FETCH_ALL, FETCH_BY_SEARCH, LIKE, START_LOADING, UPDATE } from "../constants/actionTypes";
+import { FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, START_LOADING, END_LOADING, COMMENT } from '../constants/actionTypes';
 
 export default (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
@@ -6,15 +6,28 @@ export default (state = { isLoading: true, posts: [] }, action) => {
             return { ...state, isLoading: true };
         case END_LOADING:
             return { ...state, isLoading: false };
+
         case FETCH_ALL:
             return {
                 ...state,
                 posts: action.payload.data,
                 currentPage: action.payload.currentPage,
-                numberOfPages: action.payload.numberOfPages,
+                numberOfPages: action.payload.numberOfPages
             };
         case FETCH_BY_SEARCH:
-            return { ...state, posts: action.payload };
+            return { ...state, posts: action.payload.data };
+        case FETCH_POST:
+            return { ...state, post: action.payload.post };
+        case COMMENT:
+            return {
+                ...state,
+                posts: state.posts.map((post) => {
+                    if (post._id == +action.payload._id) {
+                        return action.payload;
+                    }
+                return post;
+                }),
+            };
         case LIKE:
             return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
         case CREATE:
