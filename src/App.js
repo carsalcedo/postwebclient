686fb7cadedb7154b91ea9339afc86d014eaa821
carsalcedo <fercarrera07@gmail.com>
2023-03-13 23:22:56 -0400

    cambio de ruta

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ const App = () =>{
             <Container maxwidth= 'lg'>
                 <Navbar/>
                 <Switch>
-                    <Route path="/" exact component={() => <Redirect to='/posts' />} />
                     <Route path='/posts' exact component={Home}/>
                     <Route path='/posts/search' exact component={Home}/>
                     <Route path='/posts/:id' component={PostDetails}/>
-                   {/* <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />*/}
                     <Route path="/auth" exact component={Auth} />
+                    <Route path="/" component={() => <Redirect to='/posts' />} />
                 </Switch>
             </Container>
         </BrowserRouter>
